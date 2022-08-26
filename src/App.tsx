cb4c6f29f762f4fc012a0040b3f424f729b86c56
alonsoalex <alonsoalex5@gmail.com>
2022-08-26 12:51:49 -0300

    Push-App

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,7 @@ import style from "./App.module.scss";
 function App() {
   return (
     <div className={style.App}>
-      <header className={style["App-header"]}>
-        <img src={logo} className={style["App-logo"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={style["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
